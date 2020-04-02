ba145e94df0b7486cc7928985505a0536bb13cb1
Darjus Hosszejni <darjus.hosszejni@wu.ac.at>
2020-04-02 13:54:26 +0200

    comment code

--- download_checkbox_submissions.sh
+++ download_checkbox_submissions.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Define some course relevant variables
 unset QMGROUPS
 declare -A QMGROUPS
 QMGROUPS=(["5663"]="389262379" ["5664"]="389263201" ["5665"]="389263395" ["5666"]="389263431" ["5943"]="389262646" ["5944"]="388121092" ["5945"]="388121186" ["5946"]="388121301")
 DIR_PREFIX=Group
 
+# Download the checkbox tables
+# This actually downloads the HTML webpage that shows the table, the tab is called Submission list
 for qmgroup in "${!QMGROUPS[@]}"
 do
   wget --cookies=on --load-cookies learncookie.txt --keep-session-cookies --no-check-certificate --output-document="checkbox_Group${qmgroup}.html" --post-data="m=edit&return_url=/dotlrn/classes/qm2/${qmgroup}.20s/learning-app/en/${QMGROUPS[$qmgroup]}/prep-index#anon" "https://learn.wu.ac.at/dotlrn/classes/qm2/${qmgroup}.20s/learning-app/en/${QMGROUPS[$qmgroup]}/prep-survey$1"
 done
 
+# Turn the HTML webpage into a CSV file using xmllint and R
 for qmgroup in "${!QMGROUPS[@]}"
 do
   echo Creating ${qmgroup}.csv
--- download_group_submissions.sh
+++ download_group_submissions.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+# Define some variables specific to the course
 unset QMGROUPS
 declare -A QMGROUPS
 QMGROUPS=(["5663"]="$1" ["5664"]="$1".1 ["5665"]="$1".2 ["5666"]="$1".3 ["5943"]="$1".4 ["5944"]="$1".5 ["5945"]="$1".6 ["5946"]="$1".7)
 DIR_PREFIX=Group
 CURDIR=`pwd -P`
 
+# Download zip files (this downloads what's behind "Access student submissions")
 for qmgroup in "${!QMGROUPS[@]}"
 do
   wget --cookies=on --random-wait --load-cookies=learncookie.txt --output-document=${qmgroup}.zip --keep-session-cookies --no-check-certificate --post-data 'm=download_submissions' "https://learn.wu.ac.at/dotlrn/classes/qm2/${qmgroup}.20s/tlf-lrn/exercises/$1"
 done
 
+# Extract zip files into folders
 for qmgroup in "${!QMGROUPS[@]}"
 do
-  #ls -1 | grep "${QMGROUPS[$qmgroup]}" | xargs -I zipfile unzip zipfile -d "${DIR_PREFIX}${qmgroup}"
   unzip ${qmgroup}.zip -d "${DIR_PREFIX}${qmgroup}"
 done
 
+# Rename files as you like
 for dir in "${!QMGROUPS[@]}"
 do
-  cd "${DIR_PREFIX}${dir}"
+  cd "${CURDIR}/${DIR_PREFIX}${dir}"
   for pdf in *.pdf
   do
     mv "${pdf}" "${pdf:0:6}.pdf"
